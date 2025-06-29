1dd157be7decbb2b9eaa5092c5b8886a231f0585
Jose Ochoa <joseom41@gmail.com>
2025-06-29 09:17:36 -0500

    semana05-02-javascript

---
+++ semana05/dia01-01-javascript/main.js
@@ -0,0 +1,377 @@
+// Comentarios
+
+// console.log('Hola JS desde main.js')
+
+/*
+  Comentario
+  de
+  múltiples
+  líneas
+*/
+
+// Declarar variables (Guardan información) con let y const
+
+// NOTA: Usemos let para valores que cambian y const para valores fijos.
+
+const pi = 3.1415 // Es obligatorio inicializarlo
+let nombre // No es obligatori inicializarlo
+
+console.log(nombre) // Undefined
+
+nombre = 1500
+console.log(nombre) // 1500
+nombre = 'Victor'
+
+console.log(pi * 3)
+console.log(nombre) // Victor
+
+// Tipos de datos
+
+// PRIMITIVOS: Number, String, Boolean(true, false), undefined, null. BigInt, Symbol, etc.
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt
+
+// NUMBER
+
+console.log(20)
+console.log(15.69)
+console.log(0.9999)
+console.log(-39)
+
+console.log(typeof 20) // number
+console.log(typeof 15.69) // number
+console.log(typeof 0.9999) // number
+console.log(typeof -39) // number
+
+// STRING
+
+console.log('Claudia')
+console.log("Jose")
+console.log("200")
+console.log("🤪😍😤😵😂") // La codificación UTF-8
+
+console.log(typeof 'Claudia')
+console.log(typeof "Jose")
+console.log(typeof "200")
+console.log(typeof "🤪😍😤😵😂") // La codificación UTF-8
+
+// BOOLEAN -> false o true
+
+console.log(true)
+console.log(typeof false)
+
+// UNDEFINED
+
+let name
+console.log(name) // Hasta aquí la variable name no se le asigno un valor por eso es undefined
+console.log(typeof name)
+
+// NULL
+
+let lastName = null
+console.log(lastName)
+console.log(typeof lastName)
+
+// BIGINT -> Permite almacenar números muy grandes.
+// SYMBOL -> Ayudan a crear valores únicos e irrepetibles.
+
+// NOTA: typeof devuelve el tipo de dato de una variable o valor
+
+// OPERADORES MATEMÁTICOS
+
+console.log(1 + 2)
+console.log(3 - 2)
+console.log(2 * 2)
+console.log(1 / 2)
+console.log(1 % 2) // Residuo
+console.log(2 ** 3) // Dos elevado al cubo
+console.log(Math.pow(2, 3)) // Hace lo mismo que 2 ** 3
+
+// OPERADOR DE ASIGNACIÓN (=)
+
+const genero = 'femenino'
+
+console.log(genero)
+
+// OPERADORES DE COMPARACIÓN
+
+// Operador de igualdad y desiguadad NO ESTRICTA (==, !=)
+// Devuelve un valor booleano (true o false)
+// Este operador no toma en cuenta el tipo de dato al comparar sino solo sus valores
+
+console.log(1 == 1) // true
+console.log(1 == '1') // true ❓❓
+console.log(1 != '1') // false ❓❓
+
+// Operador de igualdad y desigualdad ESTRICTA (===, !==) (ES RECOMENDABLE USARLO CASI SIEMPRE)
+// Devuelve un valor booleano (true o false)
+// Este operado si toma en cuenta el tipo de datos y sus valores al comparar
+
+console.log(1 === 1) // true ✅
+console.log(1 === '1') // false ✅ 
+console.log(1 !== '1') // true ✅
+
+// OPERADORES DE COMPARACIÓN (siempre devuelven un booleano)
+
+console.log(8 > 5)
+console.log(5 < 1)
+console.log(8 >= 5)
+console.log(5 <= 1)
+
+// OPERADORES LÓGICOS (AND, OR, NEGACIÓN)
+
+console.log(true && false) // AND
+console.log(true || false) // OR
+console.log(!true) // NEGACIÓN
+
+// OPERADORES DE CADENA (CONCATENACIÓN)
+
+const saludo = 'Hola, '
+const nombreCompleto = 'Victor' + ' ' + 'Villazón'
+const miEdad = 39
+
+const imprimirSaludo = saludo + nombreCompleto + ". Tengo " + miEdad + ' años'
+
+console.log(nombreCompleto)
+console.log(imprimirSaludo)
+
+// EJERCICIOS
+
+// 1. Retorna true si dos strings tienen la misma longitud sino devolver false
+
+const cadena1 = 'codigox'
+const cadena2 = 'cadaga'
+
+console.log(cadena1.length)
+console.log(cadena2.length)
+
+console.log(cadena1.length === cadena2.length)
+
+// 2. (TODO) Retornar true si un numero es menor que 40 sino devolver false
+
+const num1=20
+const num2=40
+
+console.log(num1 <num2)
+
+// 3. (TODO) Retornar True si un numero es menor que 60 sino devolver False
+
+const num3=70
+const num4=60
+
+console.log(num3<num4)
+
+// 4. (TODO) Retornar True si un numero es par sino devolver False
+
+const num5=12
+console.log(num5%2 === 0)
+
+// 5. (TODO) Retornar True si un numero es impar sino devolver False
+
+const num6=13
+console.log(num6%2 !== 0)
+// 6. (TODO) calcular el area de un triangulo y mostrar su resultado si tenemos los valores base y altura
+// 7. (TODO) Almacenar en una constante un número de 3 cifras y mostrar la suma de sus cifras elevada al cubo.
+// 8. (TODO) Almacenar en una constante un monto de dinero, luego mostrar cuanto le toca a cada socio según la siguiente tabla:
+// SOCIO A = 30%, SOCIO B = 20%, SOCIO C = 60%
+
+
+// Scope (Ámbito)
+
+// Define donde una variable existe.
+// Tenemos dos tipos: Global(Fuera de las funciones) y Local (Dentro de funciones o bloques de código)
+
+let global = 'VARIABLE GLOBAL'
+
+function prueba() {
+  let local = 'VARIABLE LOCAL'
+
+  console.log('IMPRIMIENDO DESDE UN BLOQUE:', local)
+}
+
+console.log(global)
+// console.log(local) // ❌ Uncaught ReferenceError: local is not defined
+prueba()
+
+
+// CONDICIONALES (IF)
+
+if (true) {
+  // El bloque que se ejecuta si la condición es verdadera (true)
+}
+
+const numero = 33
+
+const esPar = numero % 2 === 0 // boolean
+
+if (esPar) {
+  console.log('>>> Este número es par', numero)
+}
+
+// CONDICIONALES (IF, ELSE)
+
+if (true) {
+  // Se ejecuta este bloque si la condición es verdadera
+} else {
+  // Se ejecuta este bloque si la condición es falsa
+}
+
+if (esPar) { // true, 1, 'hola'
+  console.log('Este número es par', numero)
+} else {
+  console.log('Este número es impar', numero)
+}
+
+// CONDICIONALES (IF, ELSE IF, ELSE)
+
+if (false) {
+  // Se ejecuta este bloque si la condición es verdadera
+} else if (false) {
+  // Se ejecuta este bloque si la condición es verdadera
+} else if (false) {
+  // Se ejecuta este bloque si la condición es verdadera
+} else {
+  // Se ejecuta este bloque si ninguna condición se cumple
+}
+
+let heroe = 'Spiderman'
+
+if (heroe === 'Batman') {
+  console.log('Hola soy Bruce')
+} else if (heroe === 'Spiderman') {
+  console.log('Hola soy Peter')
+} else if (heroe === 'Ironman') {
+  console.log('Hola soy Tony')
+} else {
+  console.log('No soy un heroe 😒')
+}
+
+// CONDICIONALES (SWITCH)
+
+heroe = 'Ironman'
+
+switch (heroe) {
+  case 'Batman':
+    console.log('Hola soy Bruce')
+    break
+  case 'Spiderman':
+    console.log('Hola soy Peter')
+    break
+  case 'Ironman':
+    console.log('Hola soy Tony')
+    break
+  default:
+    console.log('No soy un heroe 😒')
+}
+
+// ESTRUCTURAS REPETITIVAS
+
+// FOR (Sirve para repetir una o varias instrucciones)
+
+for(let i = 0; i < 10; i++) {
+  console.log('for', i)
+}
+
+// WHILE
+
+let j = 0
+
+while (j < 10) {
+  console.log('while', j)
+  // j = j + 1
+  j++
+}
+
+// DO WHILE
+
+let k = 0
+
+do {
+  console.log('do while', k)
+  k = k + 1
+  // k++
+} while (k < 10)
+
+// EJERCICIOS
+
+// 1. Determinar si la edad de una persona es mayor o menor de edad mostrando 'Mayor de edad' o 'Menor de edad' según corresponda.
+
+const edadPersona = 15
+
+if (edadPersona >= 18) {
+  console.log('Mayor de edad')
+} else {
+  console.log('Menor de edad')
+}
+
+// 2. Retornar un saludo en tres diferentes lenguajes:
+//    - si es español mostrará 'Hola'
+//    - si es inglés mostrará 'Hello'
+//    - si es aimara mostrará 'kamisaraki'
+
+lenguaje = 'aimara'
+
+switch (lenguaje) {
+  case 'español':
+    console.log('Hola')
+    break
+  case 'ingles':
+    console.log('Hello')
+    break
+  case 'aimara':
+    console.log('kamisaraki')
+    break
+  default:
+    console.log('No conozco el lenguaje')
+}
+// 3. Si un numero es divisible entre 3 y 5 devolver 'fizzbuzz'
+//    Si un numero es divisible entre 3 devolver 'fizz'
+//    Si un numero es divisible entre 5 devolver 'buzz'
+//    De lo contrario devolver el mismo número
+
+const numDivisible = 1
+
+if (numDivisible % 3 === 0 && numDivisible % 5 === 0) {
+  console.log('fizzbuzz')
+} else if (numDivisible %3 === 0) {
+  console.log('fizz')
+}else if (numDivisible %5 === 0){
+    console.log('buzz')
+}else{
+    console.log(numDivisible)
+}
+// 4. Retornar true si un numero es primo sino devolver false
+//    Pista: un numero primo es divisible por sí mismo y por 1
+
+
+// 5. Devolver un valor que se incremente de dos en dos y devuelva los menores a 10
+
+//funciones
+
+//definir una funcion
+
+function miFuncion(){
+    //el bloque codigo a ejecutar
+}
+
+// llamado / ejecucion de una funcion 
+
+miFuncion()
+
+//fuciones sin retorno
+
+/function esMayorOMenorDeEdad(){
+    if(edad >= 18){
+        console.log('mayor de edad')
+    }else{
+        console.log('menor de edad')
+    }
+}
+
+esMayorOMenorDeEdad(38)
+esMayorOMenorDeEdad(8)
+esMayorOMenorDeEdad(3)
+
+
+// funciones con retorno
+
---
+++ semana05/dia01-02-javascript-arrays-objetos/main.js
@@ -0,0 +1,86 @@
+// console.log('Hola JS')
+
+// TIPOS DE DATOS NO PRIMITIVOS
+
+// ARRAYS
+
+// Lista de datos ordenados. Un arreglo tiene elementos de cualquier tipo de dato: cadena, números, boolean, null, arrays, objetos, etc.
+
+// DECLARARACIÓN
+
+const arregloVacio = []
+let listaDeNumeros = [25, 36, 89, 1560]
+const listaDeValores = [1, 2 , 3, 'Victor', 'Villazón', true, null, undefined, [9, 6, 2]]
+
+console.log(arregloVacio)
+console.log(listaDeNumeros);
+console.log(listaDeValores)
+
+// Lectura de la elemento de un arreglo
+
+console.log(listaDeValores[0]) // 1
+console.log(listaDeValores[4]) // Villazón
+console.log(listaDeValores[5]) // true
+console.log(listaDeValores[99]) // undefined
+
+// Escritura en un arreglo
+
+console.log(listaDeValores)
+
+listaDeValores[0] = 'Juan'
+listaDeValores[3] = 9999
+
+console.log(listaDeValores)
+
+listaDeNumeros[0] = 'Hola'
+
+console.log(listaDeNumeros)
+
+// Insertar nuevos elementos en un arreglo al final
+
+const listaDeNombres = ['Victor', 'Alessandro', 'Francesca', 'Yesica']
+
+console.log(listaDeNombres)
+
+listaDeNombres.push('Javascript')
+listaDeNombres.push(1234)
+listaDeNombres.push(false)
+listaDeNombres.push([9, 8, 7])
+
+console.log(listaDeNombres)
+
+// Remover elementos del final del arreglo
+
+listaDeNombres.pop()
+listaDeNombres.pop()
+
+console.log(listaDeNombres)
+
+// Insertar un elemento en una posición determinada
+
+listaDeNombres.splice(0, 0, 'Código')
+listaDeNombres.splice(3, 0, 'Tecsup')
+
+console.log(listaDeNombres)
+
+// Eliminar un elemento en un posición determinada
+
+listaDeNombres.splice(3, 1, 'hola') // Eliminamos el elemento en la posición 3 y insertamos el texto 'hola' al mismo tiempo
+
+console.log(listaDeNombres)
+
+// Obtener el tamaño de nuestro arreglo
+
+console.log(listaDeNombres.length)
+console.log('bienvenido'.length)
+
+// Obtener el último elemento el arreglo
+
+console.log(listaDeNombres[listaDeNombres.length - 1])
+console.log(listaDeNombres.at(0))
+console.log(listaDeNombres[0])
+console.log(listaDeNombres.at(-2)) // Extrae el penúltimo elemento
+
+// DOC: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+
+// TODO: Otras funciones: slice (Investiguen)
